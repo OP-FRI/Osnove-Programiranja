ae114b25b579b0923dbd7387aeb6c5d0d5efc49a
Adrian Jarc <adrianjarc1@gmail.com>
2012-10-21 18:22:42 +0200

    Posodobil piramido in en file dodal
    Piramida je posodobljena ampak izpis ni povsem pravilen glede na pdf

--- Osnove Programiranja/2012/Vaje 2/PiramidaStevil.java
+++ Osnove Programiranja/2012/Vaje 2/PiramidaStevil.java
@@ -3,49 +3,21 @@ import java.util.Scanner;
 public class PiramidaStevil{
     static int x;
     static Scanner sc = new Scanner(System.in);
-    static int vrstica;
+    static int y=1;
     
     public static void main(){
         System.out.print("Vnesite višino piramide: ");
         x = sc.nextInt();
         Piramida();
     }
-
+    
     public static void Piramida(){
-        for(int i=1;i<=x;i++){
-            int d = i;
-            vrstica = i;
-            while((x-d)>0){
-                System.out.print(" ");
-                d++;
-            }
-            if (vrstica<3){
-                for(int j=i;j<=i;j++){
-                    if(j==1){
-                        System.out.print(j);
-                    }else if(j>1){
-                        while(j<=(i+2)){
-                            System.out.print(j);
-                            j++;
-                        }
-                        j=i;
-                    }
-                }
-            }else if (vrstica>=3){
-                for(int j=i;j<=i;j++){
-                    if(j==1){
-                        System.out.print(j);
-                    }else if(j>1){
-                        while(j<=(i+3)){
-                            System.out.print(j);
-                            j++;
-                        }
-                        for(int x=j;x<(j+1);++x){
-                            System.out.print(x);
-                        }
-                        j=i;
-                    }
-                }
+        for(int i=0; i<x; i++){
+            for(int j=x-i-1;j>0;j--){
+                System.out.printf(" ");
+            }for(int k=0; k<1+2*i; k++){
+                System.out.printf("%d", (y<10)?y:(y=0));
+                y++;
             }
             System.out.print("\n");
         }
