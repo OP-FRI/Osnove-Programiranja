e0ad9269e467ea0a00a24a7cdd62d4c86ab4c96e
Adrian Jarc <adrianjarc1@gmail.com>
2012-10-29 19:49:56 +0100

    Odprl folder za svojo seminarsko nalogo
    Odprl sem folder kjer se bo nahajala moja seminarska naloga ko se je bom
    lotil.

---
+++ Osnove Programiranja/2012/Seminarska naloga_AJ/Pekarna.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class Pekarna here.
+ * 
+ * @author Adrian Jarc
+ * @version 1.0
+ */
+public class Pekarna{
+    
+    private static final int ZALOGA = 10;
+    private static final int K_ZALOGA = 8;
+    private static final int NAROCILO = 10;
+    
+    public static void main(String[] args){
+            
+    }
+}
---
+++ Osnove Programiranja/2012/Seminarska naloga_AJ/README.txt
@@ -0,0 +1,2 @@
+Tukaj se bo nahajala seminarska naloga, ki jo bom izdelal.
+Seminarske naloge se bom najvrjetneje lotil v Sredo 31.10.
